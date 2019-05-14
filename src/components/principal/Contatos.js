04d24dbaab830556cd4efedc070b13da77f83a8c
Victor Magalhães <victormagalhaessales@gmail.com>
2019-05-14 13:03:02 -0300

    Adicionando TouchableHighlight nos contatos
    Adicionando TouchableHighlight nos contatos

--- src/components/principal/Contatos.js
+++ src/components/principal/Contatos.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, ListView } from 'react-native';
 import { connect } from 'react-redux';
+import { Actions } from 'react-native-router-flux'
 import { listContatosSubscribe } from './../../redux/actions/PerfilActions';
 
 class Contatos extends Component {
@@ -19,18 +20,25 @@ class Contatos extends Component {
         this.fonteDeDados = ds.cloneWithRows(contatos)
     }
 
+    renderRow(contato) {
+        return (
+            <TouchableHighlight
+                onPress={ () => Actions.conversa({ title: contato.nome, contatoNome: contato.nome, contatoEmail: contato.email }) }
+            >
+                <View style={{ flex: 1, padding: 20, borderBottomWidth: 1, borderColor: "#CCC" }}>
+                    <Text style={{ fontSize: 25 }}>{contato.nome}</Text>
+                    <Text style={{ fontSize: 18 }}>{contato.email}</Text>
+                </View>
+            </TouchableHighlight>
+        )
+    }
+
     render() {
         return (
             <ListView
                 enableEmptySections
                 dataSource={this.fonteDeDados}
-                renderRow={data => (
-                        <View style={{ flex: 1, padding: 20, borderBottomWidth: 1, borderColor: "#CCC" }}>
-                            <Text style={{ fontSize: 25 }}>{data.nome}</Text>
-                            <Text style={{ fontSize: 18 }}>{data.email}</Text>
-                        </View>
-                    )
-                }
+                renderRow={this.renderRow}
             />
         )
     }
