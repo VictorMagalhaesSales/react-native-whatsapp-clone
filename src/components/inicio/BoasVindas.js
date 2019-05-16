2e8c12b19b99f0a63718b652edc99f678ba94f5c
VictorMagalhaesSales <victormagalhaessales@gmail.com>
2019-05-16 06:56:01 -0300

    Modificando boas vindas

--- src/components/inicio/BoasVindas.js
+++ src/components/inicio/BoasVindas.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Actions } from 'react-native-router-flux';
-import { Button, View, Text, Image, StyleSheet, Dimensions, StatusBar } from 'react-native';
+import { Button, View, Text, Image, StyleSheet, Dimensions, StatusBar, TouchableHighlight, Linking } from 'react-native';
 
 export default props => (
     <Image style={style.image} source={ require('./../../imgs/bg.png')} >
         <View style={style.viewPrincipal}>
             <StatusBar backgroundColor="#114D44"/>
             <View style={{alignItems: 'center'}}>
+                <Text style={{ fontSize: 20, color: '#ffffff', marginBottom: 20 }}>Bem-vindo ao WhatsApp</Text>
                 <Image style={{width: 125, height: 125}} source={require('./../../imgs/logo.png')} />
-                <Text style={{ fontSize: 20, color: '#ffffff', marginTop: 20 }}>Seja Bem-Vindo</Text>
+                <Text style={{ fontSize: 17, color: '#ffffff', marginTop: 20, paddingLeft: 50, paddingRight: 50, textAlign: 'center' }}>Troque mensagens móveis em várias plataformas com amigos de todo o mundo</Text>
             </View>
             <View style={{width: 200}}>
                 <Button title="Fazer login" color="#115E54" onPress={() => Actions.formLogin()} />
+            </View>
+            <View>
+                <TouchableHighlight onPress={() => Linking.openURL('https://www.linkedin.com/in/victormagalhaessales/')}>
+                    <Text style={{ fontSize: 15, color: '#dbdbdb', marginTop: 20, textDecoration: 'underline'}}>Desenvolvido por Victor Magalhães</Text>
+                </TouchableHighlight>
             </View>
         </View>
     </Image>
